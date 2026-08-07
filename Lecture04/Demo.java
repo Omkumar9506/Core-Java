864ee9f79c386c981adae96528d89d10bff808af
Omkumar9506 <hari.9506563662@gmail.com>
2026-08-07 17:41:02 +0530

    lecture 4

---
+++ Lecture04/Demo.java
@@ -0,0 +1,9 @@
+package Lecture04;
+
+public class Demo {
+    public static void main(String[] args) {
+        float f=3.89f;
+        System.out.println(f);
+        System.out.printf("%.20f%n", f);
+    }
+}
