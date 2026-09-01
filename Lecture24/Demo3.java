5f724f537f45f95606be5da642b4f9306a4ce200
Omkumar9506 <hari.9506563662@gmail.com>
2026-09-01 17:47:25 +0530

    lecture 24

---
+++ Lecture24/Demo3.java
@@ -0,0 +1,29 @@
+package Lecture24;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Multiple Inheritance --> Interfaces
+
+interface A {
+    void fun();
+}
+
+interface B {
+    void fun2();
+}
+
+class C implements A, B {
+    @Override
+    public void fun() {
+
+    }
+
+    @Override
+    public void fun2() {
+        
+    }
+}
