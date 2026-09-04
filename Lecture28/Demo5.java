7614b76accf473f9d8535f235b6c06428d87fa8f
Omkumar9506 <hari.9506563662@gmail.com>
2026-09-04 15:36:01 +0530

    Lecture28

---
+++ Lecture28/Demo5.java
@@ -0,0 +1,19 @@
+package Lecture28;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        Box<String> b1 = new Box<>();
+    }
+
+    public static <T>void fun(T a, T b) { // type parameter
+
+    }
+
+}
+
+class Box<T> {
+    T value;
+}
+
+// <T> and <?>
+// super and extends
