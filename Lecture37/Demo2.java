2ab5407249690f4c4f398a550a107ddf2e0ce38a
Omkumar9506 <hari.9506563662@gmail.com>
2026-09-13 22:00:02 +0530

    Complete Lecture 37

---
+++ Lecture37/Demo2.java
@@ -0,0 +1,19 @@
+package Lecture37;
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+        Calculate c1 = (a,b) -> a + b;
+
+        System.out.println();
+    }
+
+    public void print(Calculate c1) {
+        System.out.println();
+    }
+}
+
+
+interface Calculate {
+    int sum(int a, int b);
+}
