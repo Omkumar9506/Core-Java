9f7c9da1de84567d091ad4946cd058e52abba1bc
Omkumar9506 <hari.9506563662@gmail.com>
2026-09-26 18:42:28 +0530

    Lecture 49

---
+++ Lecture49/Demo6.java
@@ -0,0 +1,17 @@
+package Lecture49;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+
+        t1.setName("worker-1");
+
+        t1.start();
+    }
+}
+
+/*
+    currentThread() --> reference of current running thread
+*/
